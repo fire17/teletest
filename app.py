ce678d92b15df28844c68782373de597d1ff424c
Tami <fire17@users.noreply.github.com>
2023-05-05 03:43:46 +0300

    Basic respond setup
    pip install python-telegram-bot, create a bot, save api key as api_key
    in env.py, Run app.py

---
+++ app.py
@@ -0,0 +1,49 @@
+from env.py import api_key
+
+from telegram.ext import *
+#import Responses as R
+
+ooo = True
+
+
+def cmd_start(update, context):
+    return "hello world "+str(update, context)
+
+def cmd_help(update, context):
+    return "helpful message :) "+str(update) + " :::" + str(context)
+    
+
+def error(update, context):
+    err = f"Update {update} caused error {context.error}"
+    print(err)
+    return err
+
+def incoming(source, *args **kwargs):
+    text = str(source.message.text)
+    print(" ::: incoming ::: ", text, args, kwargs)
+    print(source)
+    print(":::::::::::::::::::::::::::::::::::::\n\n")
+    respond = ""
+    if ooo:
+        respond = "Sorry we are not operating on the weekends. Please get back to us on sunday, Thanks and have a good one!"
+    respond = "ok ok"
+    source.message.reply_text(respond)
+    
+
+
+if __name__ == "__main__":
+    print(" ::: Welcome To Telegram Integration :::")
+    updater = Updater(api_key, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", cmd_start))
+    dp.add_handler(CommandHandler("help", cmd_help))
+    dp.add_handler(MessageHandler(Filters.text, incoming))
+    dp.add_error_handler(error)
+    
+    updater.start_polling()
+    updater.idle()
+    
+    
+print("#######################FIN######")
+ 
+    
